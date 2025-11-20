00d045a01707718a5dd9c08d1b063ed91b4df3fe
Pedro Coelho <pedro.coelho@moreharvest.com>
2025-11-20 15:41:36 +0800

    Update Vite configuration and add GitHub Pages deployment workflow
    - Changed the Vite configuration to set the base path for the application to "/lottie-editor/".
    - Introduced a new GitHub Actions workflow for deploying the application to GitHub Pages upon pushes to the main branch, including steps for building and uploading the project artifacts.

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
-import { defineConfig } from "vite";
+import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/lottie-editor/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {

8018aee80f065cda22cab9ee586fbdaab5fd6a53
Pedro Coelho <pedro.coelho@moreharvest.com>
2025-12-13 22:01:42 +0800

    fix app route

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,12 @@ import "./index.css";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router";
 
+// Get base path from Vite config (removes trailing slash if present)
+const basename = import.meta.env.BASE_URL.replace(/\/$/, "");
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={basename}>
       <App />
     </BrowserRouter>
   </StrictMode>
